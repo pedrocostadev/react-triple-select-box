47ca5cae0cbd3c874be75874eab6878db468c471
Pedro Costa <pedrodanielcc@gmail.com>
2019-08-11 00:58:26 +0100

    fix:lint

--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -1,8 +1,8 @@
 export const getSelected = options => options.filter(({ selected }) => selected)
-export const getUnselected =  options => options.filter(({ selected }) => !selected)
+export const getUnselected = options => options.filter(({ selected }) => !selected)
 
 export const getOptions = options => ({
-    left: options.left.map(({ selected, ...option }) => option),
-    center: options.center.map(({ selected, ...option }) => option),
-    right: options.right.map(({ selected, ...option }) => option)
+  left: options.left.map(({ selected, ...option }) => option),
+  center: options.center.map(({ selected, ...option }) => option),
+  right: options.right.map(({ selected, ...option }) => option)
 })
